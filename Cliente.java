0df83bfdcda8fb4effb680f9bb85a64c5a98421c
Roberth Ponce <rbrthpnc@gmail.com>
2025-05-11 10:58:39 -0600

    Cambio cliente

--- Cliente.java
+++ Cliente.java
@@ -1,8 +1,30 @@
+import java.awt.Container;
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
 class TCPClient {
  public static void main(String argv[]) throws Exception
  {
+
+  JFrame frame = new JFrame("Cálcular la nota fina del curso de calculo ");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 300);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+
+    Container contentPane = frame.getContentPane();
+    contentPane.setLayout(null);
+
+    JLabel labelTitulo = new JLabel("Ingrese la nota final del curso de calculo:");
+    labelTitulo.setBounds(10, 10, 300, 20);
+    contentPane.add(labelTitulo);
+
+    JLabel  label_P1 = new JLabel("NIngrese del parcial #1:");
+    label_P1.setBounds(10, 40, 300, 20);
+
+
+
+    
  String sentence;
  String modifiedSentence;
  BufferedReader inFromUser =
